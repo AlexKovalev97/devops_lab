15ff356c52ab2148ebf81487bdbb5490a872e656
macbook <alexanderkovalev@tut.by>
2018-07-16 15:07:45 +0300

    edited

--- homework3.py
+++ homework3.py
@@ -12,30 +12,30 @@ print(config.output)
 def log(case):
     if case == "cpu":
         return psutil.cpu_percent(1, True)
-    elif case == "vima":
+    elif case == "v_mem_available":
         return format(psutil.virtual_memory().available / 1024 ** 3, '.2f')
-    elif case == "vimu":
+    elif case == "v_mem_used":
         return format(psutil.virtual_memory().used / 1024 ** 3, '.2f')
-    elif case == "vimf":
+    elif case == "v_mem_free":
         return format(psutil.virtual_memory().free / 1024 ** 3, '.2f')
-    elif case == "duu":
+    elif case == "disk_usage_used":
         return format(psutil.disk_usage('/').used / 1024 ** 3, '.2f')
-    elif case == "duf":
+    elif case == "disk_usage_free":
         return format(psutil.disk_usage('/').free / 1024 ** 3, '.2f')
-    elif case == "dcr":
+    elif case == "disk_io_read":
         return psutil.disk_io_counters().read_count
-    elif case == "dcw":
+    elif case == "disk_io_write":
         return psutil.disk_io_counters().write_count
-    elif case == "ncps":
+    elif case == "net_pack_sent":
         return psutil.net_io_counters().packets_sent
-    elif case == "ncpr":
+    elif case == "net_pack_received":
         return psutil.net_io_counters().packets_recv
 
 
 st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 pid = psutil.Process(os.getpid())
 memoryUse = pid.memory_full_info().rss >> 10
-l = list()
+l = []
 if config.output == "txt":
     for i in range(1, 100):
         tt = time.time()
@@ -44,16 +44,17 @@ if config.output == "txt":
         memoryUse = pid.memory_full_info().rss >> 10
         print('\nSNAPSHOT', i, st, '\nCPU (%) per each core: ', log("cpu"),
               '\nVirtual Memory available/used/free : ',
-              log("vima"), 'Gb', '/',
-              log("vimu"), 'Gb', '/ ', log("vimf"),
+              log("v_mem_available"), 'Gb', '/',
+              log("v_mem_used"), 'Gb', '/ ', log("v_mem_free"),
               'Gb',
-              '\nMemory used/free : ', log("duu"),
-              ' Gb', '/', log("duf"), 'Gb',
+              '\nMemory used/free : ', log("disk_usage_used"),
+              ' Gb', '/', log("disk_usage_free"), 'Gb',
               '\nMemory used by script : ',
               memoryUse, 'kB',
-              '\nIO read count/write count : ', log("dcr"), '/ ', log("dcw"),
+              '\nIO read count/write count : ', log("disk_io_read"), '/ ',
+              log("disk_io_write"),
               '\nNetwork packets sent/packets received : ',
-              log("ncps"), '/ ', log("ncpr"),
+              log("net_pack_sent"), '/ ', log("net_pack_received"),
               file=open("log.txt", "a"))
         time.sleep(config.interval * 60)
 elif config.output == "json":
@@ -66,11 +67,11 @@ elif config.output == "json":
             'SNAPSHOT': i,
             'Timestamp': st,
             'CPU': log("cpu"),
-            'ViMemory': log("vimf"),
-            'PMemory': log("duf"),
+            'ViMemory': log("v_mem_free"),
+            'PMemory': log("disk_usage_free"),
             'Used': memoryUse,
-            'IO': log("dcw"),
-            'Network': log("ncpr"),
+            'IO': log("disk_io_write"),
+            'Network': log("net_pack_received"),
         }
         l.append(json_dict)
         time.sleep(config.interval * 60)
