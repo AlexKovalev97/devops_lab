7fdbf5a0026f91f59167bc9a23bdd084573fcab8
macbook <alexanderkovalev@tut.by>
2018-07-17 18:14:53 +0300

    Test_with_69_coverage

--- test.py
+++ test.py
@@ -9,9 +9,24 @@ class Test(unittest.TestCase):
         """Init"""
 
     def test_checker(self):
-        self.assertEqual(for_test.checker('2+3=5'), 'YES')
-        self.assertEqual(for_test.checker('1+2=4'), 'NO')
-        self.assertEqual(for_test.checker('1+3=5=2+3'), 'ERROR')
+            self.assertEqual(for_test.checker('2+3=5'), 'YES')
+
+    def test_checker_2(self):
+            self.assertEqual(for_test.checker('1+2=4'), 'NO')
+
+    def test_checker_3(self):
+            self.assertEqual(for_test.checker('1+3=5=2+3'), 'ERROR')
+
+    def test_checker_4(self):
+            self.assertEqual(for_test.checker('0123456789001234567890'
+                                              '0123456789001234567890'
+                                              '0123456789001234567890'
+                                              '0123456789001234567890'
+                                              '0123456789001234567890'
+                                              '0123456789001234567890'), 'ERROR')
+
+    def test_checker_5(self):
+        self.assertEqual(for_test.checker('1.3+5=7'), 'ERROR')
 
     def tearDown(self):
         """Finish"""
